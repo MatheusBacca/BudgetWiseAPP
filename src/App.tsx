179503b3156fde0c0732aa49cfba58814c4bb93e
MatheusBacca <matheus11bacca@gmail.com>
2024-10-10 23:07:14 -0300

    Trying to create a Screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import AppLoading from "expo-app-loading";
+import { View, Text } from 'react-native';
+
+import {
+    useFonts,
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+    Poppins_800ExtraBold
+} from "@expo-google-fonts/poppins"
+
+import { DMSans_400Regular } from "@expo-google-fonts/dm-sans";
+import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
 
 const App: React.FC = () => {
+    const [fontsLoad] = useFonts({
+        Poppins_300Light,
+        Poppins_400Regular,
+        Poppins_500Medium,
+        Poppins_700Bold,
+        Poppins_800ExtraBold,
+        DMSans_400Regular,
+        DMSerifDisplay_400Regular,
+    })
+
+    if (!fontsLoad) {
+        return <AppLoading></AppLoading>
+    }
+
     return (
         <View
             style={{
